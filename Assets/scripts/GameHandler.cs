a7f410958802a436fd1f32d992a251ab26f5c228
kristinevo <kmvo@cpp.edu>
2020-05-06 16:02:07 -0700

    added happinessBar, necessary functions added

---
+++ Assets/scripts/GameHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField] private Happiness_Bar happyBar;
+    float happiness = 1f;
+    private float nextActionTime = 0.0f;
+    public float period = .01f;
+
+
+    void Start()
+    {
+        happyBar.SetSize(happiness);
+    }
+
+    private void Update()
+    {
+        //place holder but we can make a function for happiness based on other variables
+
+        print(Time.time);
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime = Time.time + period;
+            if (happiness > 0)
+            {
+                happiness -= .1f;
+                happyBar.SetSize(happiness);
+            }
+        }        
+    }
+}
---
+++ Assets/scripts/Happiness_Bar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Happiness_Bar : MonoBehaviour
+{
+
+    private Transform happiness;
+    // Start is called before the first frame update
+    void Start()
+    {
+        happiness = transform.Find("Sprite");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        happiness.localScale = new Vector3(sizeNormalized, 1f);
+    }
+}
