f9c481e3b0041ba1b4aeda4eaf7eed877d088725
kristinevo <kmvo@cpp.edu>
2020-05-06 19:27:17 -0700

    Story intro WIP and more script

--- Assets/scripts/GameHandler.cs
+++ Assets/scripts/GameHandler.cs
@@ -1,16 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameHandler : MonoBehaviour
 {
     [SerializeField] private Happiness_Bar happyBar;
+
     float happiness = 1f;
     private float nextActionTime = 0.0f;
+
+    public Text day_Text;
+    public Text infected_Text;
+    public Text money_Text;
+
+    private int days = 0;
+    private int infected = 4570;
+    private int money = 1500;
     public float period = .01f;
 
 
     void Start()
     {
         happyBar.SetSize(happiness);
+        day_Text.text = "Days in Quarantine: " + days;
+        infected_Text.text = "Infected/Healthcare Capacity: " + infected + "/738,401";
+        money_Text.text = "Money: " + money;
     }
 
     private void Update()
@@ -24,7 +37,18 @@ public class GameHandler : MonoBehaviour
             {
                 happiness -= .1f;
                 happyBar.SetSize(happiness);
+                IncrementDay();
+                day_Text.text = "Days in Quarantine: " + days;
+                infected_Text.text = "Infected/Healthcare Capacity: " + infected + "/738,401";
+                money_Text.text = "Money: " + money;
+                day_Text.text = "Days in Quarantine: " + days;
+
             }
         }        
+    }
+
+    public void IncrementDay()
+    {
+        days++;
     }
 }
---
+++ Assets/scripts/TypeWriter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriter : MonoBehaviour
+{
+    Text txt;
+    string story;
+    void Start()
+    {
+        print("BU");
+        txt = GetComponent<Text>();
+        story = txt.text;
+        txt.text = "";
+        StartCoroutine(TypeOut());
+
+    }
+
+    public IEnumerator TypeOut()
+    {
+        foreach (char c in story)
+        {
+            txt.text += c;
+            if (c.Equals('.') || c.Equals(','))
+            {
+                yield return new WaitForSeconds(.1f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(.05f);
+            }
+        }
+    }
+}
