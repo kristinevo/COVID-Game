a66c3a188688ac3307aafa27e7c5b4ae3bcdc0fa
Allison Chen <allisonchen506@gmail.com>
2020-05-07 20:18:30 -0700

    connected the scenes
    connected the scenes

---
+++ Assets/scripts/TV_Selection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TV_Selection : MonoBehaviour
+{
+    public void onStartGame()
+    {
+        SceneManager.LoadScene("TV");
+    }
+}
