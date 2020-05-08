e1d9bc8ab34e1b3dbc089afe2db801c08e490529
kristinevo <kmvo@cpp.edu>
2020-05-07 19:52:09 -0700

    Finished the menu, could still be improved

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Button exit;
+    public Button play_Game;
+    void Start()
+    {
+        play_Game.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Introduction");
+        });
+
+        exit.onClick.AddListener(() =>
+        {
+            System.Environment.Exit(1);
+
+        });
+
+    }
+}
